1c5c1eae2fae4f587b93e1cbe12db5cfde05e5f0
Oliver Moloney <127998765+Scrapjon@users.noreply.github.com>
2025-03-24 18:17:05 +1100

    Initial commit

---
+++ extractor.py
@@ -0,0 +1,67 @@
+import cv2
+import os
+import numpy as np
+import pandas as pd
+
+
+def main():
+    frames_with_info = extract()
+    features = []
+    for frame, timestamp, frame_filename in frames_with_info:
+        hist = extract_colour_histogram(frame=frame)
+        edges = extract_edge_features(frame=frame)
+        combined = [timestamp, frame_filename] + hist.tolist() + edges.tolist()
+        features.append(combined)
+    hist_size = 8*8*8 # 8 bins per channel
+    edge_size = 1000 # data is truncated to 1000 values
+    save_features_to_csv(features=features,hist_size=hist_size,edge_size=edge_size,output_file='output_data/output_features.csv')
+    
+    
+
+
+def extract():
+    video_path = 'input_videos/input.mp4'
+    output_dir = 'output_frames'
+    os.makedirs(output_dir, exist_ok=True)
+
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+    frame_count = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        timestamp = cap.get(cv2.CAP_PROP_POS_MSEC) # timestamp in milliseconds
+        frame_filename = f"{output_dir}/frame_{frame_count}_time_{int(timestamp)}.jpg"
+        cv2.imwrite(frame_filename, frame)
+        frame_data = (frame, int(timestamp), frame_filename)
+        frames.append(frame_data)
+        frame_count = len(frames)
+
+    cap.release()
+    print(f"Extracted {frame_count} frames.")
+    return frames
+
+def extract_colour_histogram(frame):
+    hist = cv2.calcHist([frame],[0,1,2],None,[8,8,8],[0,256,0,256,0,256]) # 8 bins per channel
+    hist = cv2.normalize(hist,hist).flatten()
+    return hist
+
+def extract_edge_features(frame):
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # greyscale it
+    edges = cv2.Canny(grey,100,200)
+    return edges.flatten()[:1000] # truncate to keep it simple
+
+def save_features_to_csv(features, hist_size, edge_size, output_file):
+    #column names
+    hist_columns = [f'hist_bin_{i}' for i in range(hist_size)]
+    edge_columns = [f'edge+{i}' for i in range(edge_size)]
+    column_names = ['timestamp','frame_filename'] + hist_columns + edge_columns
+
+    df = pd.DataFrame(features, columns=column_names)
+    df.to_csv(output_file, index=False)
+    print(f"Saved combined features to {output_file}")
+
+if __name__ == '__main__':
+    main()
